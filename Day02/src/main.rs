5299f54c68a4fd4ba5e7e2003992aae7a8010d23
Martin Zimmermann <martin.zimmermann@fsm.ag>
2023-12-28 14:04:12 +0100

    Update: Part 2

--- Day02/src/main.rs
+++ Day02/src/main.rs
@@ -4,7 +4,8 @@ const MAX_RED: i32 = 12;
 const MAX_GREEN: i32 = 13;
 const MAX_BLUE: i32 = 14;
 
-fn main() {
+fn part_one()
+{
     let mut sum_of_failed_games : i32 = 0;
     let mut max_game: i32 = 0;
 
@@ -41,4 +42,42 @@ fn main() {
     let sum_100: i32 = (0..(max_game+1)).sum();
     let result_a: i32 = sum_100 - sum_of_failed_games;
     println!("Solution of part a: {}", result_a);
+}
+
+fn part_two()
+{
+    let mut sum_of_powers: i32 = 0;
+
+    for line in fs::read_to_string("input.txt").unwrap().lines(){
+        // 1. Extract game number
+        let (_, game_str) = line.split_once(':').unwrap();
+        let mut sum_of_red: i32 = 0;
+        let mut sum_of_green: i32 = 0;
+        let mut sum_of_blue: i32 = 0;
+
+        // 2. Separate each reveal by ';'
+        for sets in game_str.split(';')
+        {
+            for set in sets.split(',')
+            {
+                let (cnt, color) = set[1..].split_once(' ').unwrap();
+                let cnt = cnt.parse::<i32>().unwrap();
+                match color {
+                    "green" => sum_of_green = sum_of_green.max(cnt),
+                    "red" => sum_of_red = sum_of_red.max(cnt),
+                    "blue" => sum_of_blue = sum_of_blue.max(cnt),
+                    _ => panic!("Oh no color")
+                };
+            }
+        }
+        let power = sum_of_red * sum_of_green * sum_of_blue;
+        sum_of_powers = sum_of_powers + power;
+    }
+    println!("Solution of part b: {}", sum_of_powers);
+}
+
+fn main() {
+   part_one();
+
+   part_two();
 }
